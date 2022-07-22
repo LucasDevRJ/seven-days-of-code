322efe0155ca2d2c5f48205eef783a37194d4962
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-07-22 19:02:32 -0300

    solicitação GET que imprime o corpo da resposta como uma String

--- src/ListaFilmes.java
+++ src/ListaFilmes.java
@@ -1,4 +1,10 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 public class ListaFilmes {
 
+	HttpClient cliente = HttpClient.newHttpClient();
+	HttpRequest solicitacao = (HttpRequest) HttpRequest.newBuilder();
+	URI url = URI.create("https://imdb-api.com/en/API/Top250Movies/k_l6qb542b");
 }
